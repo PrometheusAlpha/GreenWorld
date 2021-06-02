3de238df596b2a6949e134f9ff444ae897ff34a8
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2021-06-02 20:55:11 +0700

    delete compare

--- js/components/compare.js
+++
@@ -1,5 +0,0 @@
-let slide = (e) => {
-  const sliderPos = e.target.value;
-  e.target.previousElementSibling.style.width = `${sliderPos}%`;
-  e.target.nextElementSibling.style.left = `calc(${sliderPos}% - 18px)`;
-};
