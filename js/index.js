0fc339652eb11d407bf527c49a37808a6eac60b0
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2021-06-13 20:59:05 +0700

    DESIGN

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@ function loadScript(url) {
   var body = document.getElementsByTagName("body")[0];
   var script = document.createElement("script");
   script.type = "text/javascript";
-  script.defer = true;
-  script.async = true;
   script.src = url;
   body.appendChild(script);
 }
