ecb15dbd0768d09ad4e0d409fad95c59547e7cf4
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2021-06-13 20:50:55 +0700

    DESIGN

--- js/components/post.js
+++ js/components/post.js
@@ -1,10 +1,12 @@
-function show(type){
-    var id = type + "_post";
-    var target = document.getElementById(id);
-    target.style.display = "block";
+function show(type) {
+  var id = type + "_post";
+  var target = document.getElementById(id);
+  target.style.display = "block";
+  document.getElementsByTagName("body")[0].style.overflow = "hidden";
 }
-function hide(type){
-    var id = type + "_post";
-    var target = document.getElementById(id);
-    target.style.display = "none";
+function hide(type) {
+  var id = type + "_post";
+  var target = document.getElementById(id);
+  target.style.display = "none";
+  document.getElementsByTagName("body")[0].style.overflow = "auto";
 }
