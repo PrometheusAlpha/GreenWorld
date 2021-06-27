cbfab1fee07387ac33fc3ff15d10743a98691ad6
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2021-06-27 17:17:01 +0700

    .

--- js/components/post.js
+++ js/components/post.js
@@ -1,1 +1,14 @@
-function show(type) { var id = type + "_post"; var target = document.getElementById(id); target.style.display = "block"; document.getElementsByTagName("body")[0].style.overflow = "hidden"; document.getElementsByTagName("nav")[0].style.display = "none"; } function hide(type) { var id = type + "_post"; var target = document.getElementById(id); target.style.display = "none"; document.getElementsByTagName("body")[0].style.overflow = "auto"; document.getElementsByTagName("nav")[0].style.display = ""; }
+function show(type) {
+  var id = type + "_post";
+  var target = document.getElementById(id);
+  target.style.display = "block";
+  document.getElementsByTagName("body")[0].style.overflow = "hidden";
+  document.getElementsByTagName("nav")[0].style.display = "none";
+}
+function hide(type) {
+    var id = type + "_post";
+    var target = document.getElementById(id);
+    target.style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+    document.getElementsByTagName("nav")[0].style.display = "";
+}
