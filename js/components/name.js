3177b4915b8930269ea4decc3d2e1132dccec8fe
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2021-06-27 17:09:15 +0700

    .

--- js/components/name.js
+++ js/components/name.js
@@ -1,8 +1,1 @@
-function names(text1){
-    var target = document.getElementById("name");
-    target.innerHTML = text1;
-}
-function unname(){
-    var target = document.getElementById("name");
-    target.innerHTML = "";
-}
+function names(text1) { var target = document.getElementById("name"); target.innerHTML = text1; } function unname() { var target = document.getElementById("name"); target.innerHTML = ""; }
--- js/components/nav.js
+++ js/components/nav.js
@@ -1,16 +1,1 @@
-window.onscroll = function (f) {
-  var logo_left = document.getElementById("logo__disappear__left");
-  var logo_right = document.getElementById("logo__disappear__right");
-
-  if (this.oldScroll < this.scrollY) {
-    logo_left.style.marginRight = "-80px";
-    logo_left.style.opacity = "0";
-    logo_right.style.opacity = "0";
-  } else {
-    logo_left.style.marginRight = "0";
-    logo_left.style.opacity = "1";
-    logo_right.style.opacity = "1";
-  }
-
-  this.oldScroll = this.scrollY;
-};
+window.onscroll = function (f) { var logo_left = document.getElementById("logo__disappear__left"); var logo_right = document.getElementById("logo__disappear__right"); if (this.oldScroll < this.scrollY) { logo_left.style.marginRight = "-80px"; logo_left.style.opacity = "0"; logo_right.style.opacity = "0"; } else { logo_left.style.marginRight = "0"; logo_left.style.opacity = "1"; logo_right.style.opacity = "1"; } this.oldScroll = this.scrollY; };
