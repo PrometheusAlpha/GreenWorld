9ddda6c805030cd4eb13355cfd6966a7315b4aab
promtheusalpha <hoangnguyen.hn212.hn@gmail.com>
2022-04-07 20:45:37 +0700

    FIX bug

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ function loadScript(url) {
   script.src = url;
   body.appendChild(script);
 }
+
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
@@ -18,8 +19,12 @@ function includeHTML() {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
